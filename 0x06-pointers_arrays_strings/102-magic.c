a383dc136534c15d348ceaee054f6d202de379fe
Roland Blessing <Roland.c.blessing@gmail.com>
2022-09-22 18:18:17 -0700

    A a function that adds two numbers.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
+/* 
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ */
 int main(void)
 {
 	int n;
 
 	int a[5];
 
-	int *p;
+	int *p
 
 	a[2] = 1024;
 	p = &n;
 	/*
 	 * write your line of code here...
-	 * Remember:
+	 * Remember: 
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
